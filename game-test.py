de4fb7f51b7e0008cb8591b7b8465a02fd0a6ccf
Simon <simonharrisonsiena@gmail.com>
2024-04-25 16:42:11 -0400

    fixed build and cleaned depreciated functions

--- game-test.py
+++ game-test.py
@@ -3,33 +3,24 @@ import pygame
 from pygame import Color
 import random
 
-distance = 50
-startposx = 100
-startposy = 100
+startposx = 2
+startposy = 2
+scale = 50 # Pixels per column
 
 class Food:
     def __init__(self) -> None:
-        self.x = 50*random.randint(1, 20)
-        self.y = 50*random.randint(1, 20)
-        self.surface = pygame.Surface([50,50])
-        self.surface.fill(Color("red"))
-        self.rect = self.surface.get_rect()
-        self.rect.topleft = (self.x, self.y)
-        
-        pass
-    
+        self.respawn()
     
     def respawn(self):
         self.column = random.randint(1, 20)
         self.row = random.randint(1, 20)
-        self.x = 50 * self.column
-        self.y = 50 * self.row
+        self.x = self.column * 50
+        self.y = self.row * 50
+        self.surface = pygame.Surface((50, 50))
         self.surface.fill(Color("red"))
         self.rect = self.surface.get_rect()
         self.rect.topleft = (self.x, self.y)
-        pass
         
-    
     
 
 class Segment:
@@ -37,80 +28,63 @@ class Segment:
         self.lifespan = None
         self.collision = False
         
-    def create(self, lifespan: int, x, y, head: bool = False) -> None:
-        self.column: int
-        self.row: int
+    def create(self, column, row, head: bool = False) -> None:
+        self.column = column
+        self.row = row
         self.x = self.column * 50
         self.y = self.row * 50
-        self.lifespan = lifespan
-        self.surface = pygame.Surface([50,50])
-        self.surface.fill(Color("orange"))
+        self.surface = pygame.Surface([30,30])
+        self.surface.fill(Color("grey"))
         self.rect = self.surface.get_rect()
+        self.rect.center = (self.x, self.y)
         self.is_head = head
         self.collision = False
-
-    def activate_collision(self):
-        if not self.is_head:
-            self.collision = True
-            self.surface.fill(Color('green'))
         
-        
-    def is_alive(self) -> bool:
-        self.lifespan -= 1
-        if self.lifespan <= 0:
-            return False
-        else:
-            return True
-        
+    
         
 
 class Snake:
     def __init__(self) -> None:
-        self.head = Segment()
-        self.head.create(lifespan=None, x=startposx, y=startposy, head=True) 
+        self.head_column = startposx
+        self.head_row = startposy
         self.segments = []
         self.direction = "right"
-        self.speed = 50
+        self.speed = 1
         self.length = 3
         
     def grow(self) -> None:
         self.length += 1
-        for segment in self.segments: #Iterate through all segments and extend their lifespan by one tick
-            segment.lifespan += 1
         
         
     def move(self) -> None:
-        for segment in self.segments:
-            if not segment.is_alive():
-                self.segments.pop(0)
         
         self.segments.append(Segment())
-        self.segments[-1].create(lifespan=self.length, x=self.head.x, y=self.head.y, head=False)
-        
+        self.segments[-1].create(column=self.head_column, row=self.head_row, head=False)
         
         
         if self.direction == "right":
-            self.head.x += self.speed
+            self.head_column += self.speed
         elif self.direction == "left":
-            self.head.x -= self.speed
+            self.head_column -= self.speed
         elif self.direction == "up":
-            self.head.y -= self.speed
+            self.head_row -= self.speed
         elif self.direction == "down":
-            self.head.y += self.speed
+            self.head_row += self.speed
         
-        
-        pass
+        if len(self.segments) > self.length:
+            self.segments.pop(0)
+
     
     
     
     def check_collision(self):
-        if self.head.x > 1000 or self.head.x < 0 or self.head.y > 1000 or self.head.y < 0:
-            print('Collision')
+        if self.head_column > 20 or self.head_column < 0 or self.head_row > 20 or self.head_row < 0:
+            print('Off Screen')
             exit()
         
         for segment in self.segments:
-            if self.head.rect.colliderect(segment.rect) and segment.collision:
-                print('Collision with snake body')
+            if self.head_column == segment.column and self.head_row == segment.row:
+                print('Hit itself')
                 exit()
         
         
@@ -141,13 +115,11 @@ def main():
              
     
     
-    
-    screen.blit(snake.head.surface, (50,50))
+
     # define a variable to control the main loop
     running = True
     
-    
-    pygame.display.flip()
+
     # main loop
     while running:
         # event handling, gets all event from the event queue
@@ -177,22 +149,26 @@ def main():
             # check if the smiley is still on screen, if not change direction
 
         
-        
+
         snake.move()
-        snake.check_collision()
+                        
+
+                    
         
-        if snake.head.rect.colliderect(food.rect):
-            food.respawn()
-            snake.grow()
-        
-        pygame.draw.rect(screen,(200, 180, 90), [0, 0 , 1000, 1000])
-        screen.blit(snake.head.surface, (snake.head.x,snake.head.y))
+        pygame.draw.rect(screen,(100, 100, 90), [0, 0 , 1000, 1000])
             
         for segment in snake.segments:
-            screen.blit(segment.surface, (segment.x, segment.y))
+            
+            if food.column == segment.column and food.row == segment.row:
+                snake.grow()
+                food.respawn()
+        
+            screen.blit(segment.surface, (segment.column * scale, segment.row * scale))
         screen.blit(food.surface, (food.x, food.y))
         pygame.display.flip()
-        pygame.time.delay(100)
+        snake.check_collision()
+        pygame.time.delay(75)
+        
 # run the main function only if this module is executed as the main script
 # (if you import this as a module then nothing is executed)
 if __name__=="__main__":
